dae245e81d4166f6c35e795f59e845bd1293f911
Gang-gege <2453451606@qq.com>
2021-06-14 22:09:13 +0800

    444

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,5 @@ module.exports = {
     // 默认值是 '/' 就是服务器根地址 加入你的服务器地址是 https://<USERNAME>.github.io/<REPO>/ 那么publicPath 默认就是 https://<USERNAME>.github.io/ 当你的 index.html 导入资源的时候路径就是错误的
 
     // 要区分生产环境和开发环境， 只有在部署 的时候也就是生产环境下需要设置 publicPath 
-    publicPath: process.env.NODE_ENV === 'production' ? '/cnode11/' : '/'
+    publicPath: process.env.NODE_ENV === 'production' ? '/Automan/' : '/'
 }
